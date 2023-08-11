018fb553d0bae54b4ef03d6930ef18c3d3375b6c
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-11 21:03:41 +0300

    57-BE-create APIView

--- electrodom/goods/views.py
+++ electrodom/goods/views.py
@@ -2,10 +2,18 @@ from rest_framework.viewsets import ModelViewSet
 
 from goods.models import Categories
 from goods.serializers import CategorySerializer
+from goods.serializers import ProviderSerializer
+
+from goods.models import Providers
 
 
 class CategoriesViewSet(ModelViewSet):
     queryset = Categories.objects.all()
     serializer_class = CategorySerializer
+
+
+class ProvidersViewSet(ModelViewSet):
+    queryset = Providers.objects.all()
+    serializer_class = ProviderSerializer
 
 
