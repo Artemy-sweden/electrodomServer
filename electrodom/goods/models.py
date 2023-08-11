a6146d1ad8b0133bbdaefbefd51db3076dc610ed
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-11 20:29:38 +0300

    55-BE-finish realize CRUD

--- electrodom/goods/models.py
+++ electrodom/goods/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 
-class DBMixin(models.Model):
-    using = 'default'  # По умолчанию, используем default базу данных
+# class DBMixin(models.Model):
+#     # using = 'default'  # По умолчанию, используем default базу данных
+#
+#     class Meta:
+#         abstract = True
+#
+#     def save(self, *args, **kwargs):
+#         using_db = getattr(self, 'using', self.using)
+#         super().save(using=using_db, *args, **kwargs)
 
-    class Meta:
-        abstract = True
 
-    def save(self, *args, **kwargs):
-        using_db = getattr(self, 'using', self.using)
-        super().save(using=using_db, *args, **kwargs)
-
-
-class Providers(DBMixin):
+class Providers(models.Model):
     provider_name = models.CharField(max_length=255)
 
 
-class Categories(DBMixin):
+class Categories(models.Model):
     category_name = models.CharField(max_length=255)
 
 
-class Goods(DBMixin):
+class Goods(models.Model):
     name = models.CharField(max_length=255)
     provider_id = models.ForeignKey(to=Providers, on_delete=models.CASCADE, null=True)
     category_id = models.ForeignKey(to=Categories, on_delete=models.CASCADE, null=True)
