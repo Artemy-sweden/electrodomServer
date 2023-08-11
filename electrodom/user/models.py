c436dde0687c76a80251751aa220a0a48fbb2c71
VladNevgod <vladnewgod@gmail.com>
2023-08-11 14:38:11 +0300

    22-BE-Add metaclass to user models

--- electrodom/user/models.py
+++ electrodom/user/models.py
@@ -10,14 +10,26 @@ class User(models.Model):
     phone_number = models.CharField(max_length=13)
     address = models.TextField()
 
+    class Meta:
+        app_label = 'user'
+        using = 'users'
+
 
 class Comment(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False)
     comment_text = models.TextField(verbose_name='Текст')
     comment_date = models.DateTimeField(verbose_name='Дата создания')
 
+    class Meta:
+        app_label = 'user'
+        using = 'users'
+
 
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False)
     goods = models.ForeignKey(to=Goods, on_delete=models.CASCADE, null=False)
     count = models.PositiveSmallIntegerField()
+
+    class Meta:
+        app_label = 'user'
+        using = 'users'
