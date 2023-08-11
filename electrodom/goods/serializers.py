1f3450cf1655c83eb2ca501e5409ca9668cbc787
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-11 21:23:00 +0300

    53-BE-create serializer for model

--- electrodom/goods/serializers.py
+++ electrodom/goods/serializers.py
@@ -3,6 +3,8 @@ from goods.models import Categories
 
 from goods.models import Providers
 
+from goods.models import Goods
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +16,9 @@ class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Providers
         fields = ['id', 'provider_name']
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = ['id', 'name', 'provider_id', 'category_id', 'price', 'count', 'description']
