02ca73145c7a0f2eea4f29d05f3dc6d8d30670d8
VladNevgod <vladnewgod@gmail.com>
2023-08-11 13:59:45 +0300

    22-BE-Create user models

--- electrodom/user/models.py
+++ electrodom/user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from electrodom.goods.models import Goods
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to='users_photos')
+    phone_number = models.CharField(max_length=13)
+    address = models.TextField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False)
+    comment_text = models.TextField(verbose_name='Текст')
+    comment_date = models.DateTimeField(verbose_name='Дата создания')
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False)
+    goods = models.ForeignKey(to=Goods, on_delete=models.CASCADE, null=False)
+    count = models.PositiveSmallIntegerField()
