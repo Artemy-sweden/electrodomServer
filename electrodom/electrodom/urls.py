56baee18c90ef8aeec10f038039498e921684f15
VladNevgod <vladnewgod@gmail.com>
2023-08-11 20:35:23 +0300

    52-BE-Create router in urls.py for user model

--- electrodom/electrodom/urls.py
+++ electrodom/electrodom/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from user.views import UserViewSet
+
+r = DefaultRouter()
+r.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+] + r.urls
