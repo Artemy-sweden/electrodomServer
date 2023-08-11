bc88804438592924aa193e504d651fbb5e4765c6
VladNevgod <vladnewgod@gmail.com>
2023-08-11 14:38:01 +0300

    22-BE-Add metaclass to goods models

--- electrodom/goods/models.py
+++ electrodom/goods/models.py
@@ -4,9 +4,17 @@ from django.db import models
 class Providers(models.Model):
     provider_name = models.CharField(max_length=255)
 
+    class Meta:
+        app_label = 'goods'
+        using = 'default'
+
 
 class Categories(models.Model):
     category_name = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'goods'
+        using = 'default'
 
 
 class Goods(models.Model):
@@ -16,3 +24,7 @@ class Goods(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=6)
     count = models.PositiveIntegerField()
     description = models.TextField()
+
+    class Meta:
+        app_label = 'goods'
+        using = 'default'
