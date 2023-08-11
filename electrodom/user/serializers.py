103c1566a32df3fd3c25c72b99454a92985e3d23
VladNevgod <vladnewgod@gmail.com>
2023-08-11 21:14:30 +0300

    56-BE-Create serializer for Comment model

--- electrodom/user/serializers.py
+++ electrodom/user/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from user.models import User
+from user.models import User, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'photo', 'phone_number', 'address']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'comment_text', 'comment_date']
