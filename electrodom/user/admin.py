a699d955ba5bea355d8f6c1e310580ee60ff26e5
VladNevgod <vladnewgod@gmail.com>
2023-08-11 21:15:54 +0300

    56-BE-Create a CRUD for Comment and linked with the admin panel

--- electrodom/user/admin.py
+++ electrodom/user/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from user.models import User
+from user.models import User, Comment
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', 'photo', 'phone_number', 'address']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ['user', 'comment_text', 'comment_date']
