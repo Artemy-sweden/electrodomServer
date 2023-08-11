3af8e7e3d9774bb5300b4fe2d8de03a4518a8449
VladNevgod <vladnewgod@gmail.com>
2023-08-11 21:29:42 +0300

    58-BE-Create the router and path for Basket

--- electrodom/electrodom/urls.py
+++ electrodom/electrodom/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from user.views import UserViewSet, CommentViewSet
+from user.views import UserViewSet, CommentViewSet, BasketViewSet
 from goods.views import CategoriesViewSet
 from goods.views import ProvidersViewSet
 
@@ -13,6 +13,7 @@ router.register('categories', CategoriesViewSet)
 router.register('users', UserViewSet)
 router.register('comments', CommentViewSet)
 router.register('providers', ProvidersViewSet)
+router.register('baskets', BasketViewSet)
 
 
 urlpatterns = [
