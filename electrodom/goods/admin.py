8533d4dd1cfc352a94c2d49112930b052d4360a0
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-11 21:11:00 +0300

    57-BE-register and finish

--- electrodom/goods/admin.py
+++ electrodom/goods/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from goods.models import Categories
 
+from goods.models import Providers
+
 
 @admin.register(Categories)
 class SitePhotosAdmin(admin.ModelAdmin):
     fields = ('category_name', )
+
+
+@admin.register(Providers)
+class SitePhotosAdmin(admin.ModelAdmin):
+    fields = ('provider_name', )
