e9466256e8008f95b39b12aa62203b112a951799
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-15 15:25:12 +0300

    72-BE-add extra_kwargs

--- electrodom/user/serializers.py
+++ electrodom/user/serializers.py
@@ -3,19 +3,26 @@ from rest_framework import serializers
 from user.models import User, Comment, Basket
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'photo', 'phone_number', 'address']
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'comment_text', 'comment_date']
         read_only_fields = ['user']
 
+
 class BasketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Basket
         fields = ['id', 'user', 'goods', 'count']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    baskets = BasketSerializer(many=True, source='baskets_set', required=False)
+
+    extra_kwargs = {
+        'password': {'write_only': True}
+    }
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'photo', 'phone_number', 'address', 'baskets']
