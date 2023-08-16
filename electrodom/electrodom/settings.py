8306d137709950241e5be3fc29f85a83285111b3
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-08-16 21:52:22 +0300

    create req

--- electrodom/electrodom/settings.py
+++ electrodom/electrodom/settings.py
@@ -29,7 +29,7 @@ from corsheaders.defaults import default_methods
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django-insecure-_&de1ejn6r322c#+k=8v233#1l$-7(pu7ra^bt3v$n5#-l-$+1
+SECRET_KEY = 'django-insecure-_&de1ejn6r322c#+k=8v233#1l$-7(pu7ra^bt3v$n5#-l-$+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
