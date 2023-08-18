8f38c08039eaa0dd51bdf170ac0ddd54b252c24a
VladNevgod <vladnewgod@gmail.com>
2023-08-18 17:15:03 +0300

    78-Edit users serializer

--- electrodom/user/serializers.py
+++ electrodom/user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from user.models import User, Comment, Basket
+from user.models import Basket, User, Comment
+from goods.serializers import GoodsSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -11,13 +12,15 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class BasketSerializer(serializers.ModelSerializer):
+    goods = GoodsSerializer(required=False)
+
     class Meta:
         model = Basket
         fields = ['id', 'user', 'goods', 'count']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    baskets = BasketSerializer(many=True, source='baskets_set', required=False)
+    baskets = BasketSerializer(many=True, source='basket_set', required=False)
 
     extra_kwargs = {
         'password': {'write_only': True}
@@ -26,5 +29,5 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'password', 'photo', 'phone_number', 'address', 'baskets']
-        ref_name = 'CustomUserSerializer'  # Уникальное имя для этого сериализатора
+
 
