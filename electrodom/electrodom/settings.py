15d24b4f99b9fa8d0f2059985ea119fee5eec41d
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-09-04 08:43:57 +0300

    create logging

--- electrodom/electrodom/settings.py
+++ electrodom/electrodom/settings.py
@@ -178,3 +178,19 @@ REST_FRAMEWORK = {
     }
 
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    },
+}
